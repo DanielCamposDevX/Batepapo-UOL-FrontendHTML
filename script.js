cc42576a090b4a2f8142db7f4a957a238198fe34
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-04-17 09:43:01 -0300

    versao padrao sem bonus

--- script.js
+++ script.js
@@ -7,32 +7,55 @@ const nome = prompt('Qual é o seu nome?');
 const entrando = {
     name: nome,
 };
+
 const nomepromise = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', entrando);
 nomepromise.then(participants);
 nomepromise.catch(Deuruim);
 setInterval(msgload, 3000);
 
 
+/*carrega mensagens*/
 function msgload() {
     const msgsearch = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages')
     msgsearch.then(msgrender);
 }
 
+/*renderiza mensagens*/
 function msgrender(msgs) {
     const chat = document.querySelector('.chat');
+    const barra = document.querySelector('.bottom');
     chat.innerHTML = '';
     const quantas = msgs.data.length;
     for (i = 0; i < quantas; i++) {
         let mensage = msgs.data[i];
-        if(mensage.type == 'message'){
-        chat.innerHTML += '<div class="msg"><div class="hour">' + mensage.time + '</div> <div class="user">' + mensage.from + '</div> <div class="texto"> para  ' +mensage.to +': '+ mensage.text + '</div></div>';
+        if (mensage.type == 'message') {
+            chat.innerHTML += '<div class="msg"  data-test="message"><div class="hour">' + mensage.time + '</div> <div class="user">' + mensage.from + '</div> <div class="texto1"> para  ' + mensage.to + ': </div> <div class="texto">' + mensage.text + '</div></div>';
         }
-        else if(mensage.type == 'status'){
-        chat.innerHTML += '<div class="msgstatus"><div class="hour">' + mensage.time + '</div> <div class="user">' + mensage.from + '</div> <div class="texto">'+ mensage.text + '</div></div>';
+        else if (mensage.type == 'status') {
+            chat.innerHTML += '<div class="msgstatus"  data-test="message"><div class="hour">' + mensage.time + '</div> <div class="user">' + mensage.from + '</div> <div class="texto">' + mensage.text + '</div></div>';
         };
-        chat.scrollIntoView({ block: "end", inline: "nearest" });
+        barra.scrollIntoView({ block: "end", inline: "nearest" });
     }
+}
+
+/*envia mensagem*/
+function sendmsg() {
+    let msgtext = document.querySelector('.escreva').value;
+    const msg = {
+        from: nome,
+        to: "Todos",
+        text: msgtext,
+        type: "message"
     }
+    const promisse = axios.post('https://mock-api.driven.com.br/api/vm/uol/messages', msg);
+    promisse.then = msgload();
+    promisse.catch = window.location.reload();
+    msgtext = '';
+}
+
+
+
+
 
 
 
@@ -41,7 +64,7 @@ function participants() {
     console.log(nomepromise)
     const chat = document.querySelector('.chat');
 
-    chat.innerHTML += '<div class="msgstatus"><div class="user">' + entrando.name + '</div> <div class="texto">Entrou na sala</div> </div>';
+    chat.innerHTML += '<div class="msgstatus"  data-test="message" ><div class="user">' + entrando.name + '</div> <div class="texto">Entrou na sala</div> </div>';
     console.log(chat);
     setInterval(atividade, 5000);
 }
@@ -50,14 +73,20 @@ function participants() {
 
 /*nome está em uso*/
 function Deuruim() {
-    const nome = prompt('Qual é o seu nome?');
+    /*const nome = prompt('Qual é o seu nome?');
     const entrando = {
         name: nome,
     };
     const nomepromise1 = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', entrando);
     nomepromise1.then(participants);
-    nomepromise1.catch(Deuruim);
+    nomepromise1.catch(Deuruim);*/
+    window.location.reload();
 }
+
+
+
+
+
 
 /*manter online*/
 function atividade() {
